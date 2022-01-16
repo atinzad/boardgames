792d62677c0f1f7024b4637c3ee72fdbd4311ec5
Ahmad <ahmad_baq@hotmail.com>
2022-01-16 19:23:26 +0300

    added a products.js file with products array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {products as prod, items as itm} from "./products"
+
 
 function App() {
-  return (<div className ="container">
+  return (<div className ="App">
   <h1> Hello World to Board Games</h1>
   <p>site for buying and selling Board Games!</p>
-  <img className="displayed" alt="Board Game logo" src="https://cf.geekdo-static.com/images/logos/navbar-logo-bgg-b2.svg"/>
+  <img alt="Board Game logo" src="https://cf.geekdo-static.com/images/logos/navbar-logo-bgg-b2.svg"/>
+  <h1> {prod[0].name}</h1>
+  <h2>{prod[0].price}</h2>
+  <img src={prod[0].image} />
+  <h1> {prod[1].name}</h1>
+  <h2>{prod[1].price}</h2>
+  <img src={prod[1].image} />
   </div>
   );
 }
