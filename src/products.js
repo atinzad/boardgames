5e8136cea1c15227e22e0988d10ba03699f36833
Ahmad <ahmad_baq@hotmail.com>
2022-01-16 19:36:58 +0300

    fixed image in products

--- src/products.js
+++ src/products.js
@@ -1,27 +1,29 @@
+export const products = [
+  {
+    name: "Blood Rage",
+    price: 50.0,
+    image:
+      "https://cf.geekdo-images.com/HkZSJfQnZ3EpS214xtuplg__imagepage/img/nLp0poXg-Y6szkicHe7U2thnwhk=/fit-in/900x600/filters:no_upscale():strip_icc()/pic2439223.jpg",
+  },
+  {
+    name: "Rising Sun",
+    price: 60.0,
+    image:
+      "https://cf.geekdo-images.com/iwevA6XmiNLHn1QnGUucqw__imagepage/img/W6vxWVStCBL7qs6yW_G0VbC9Jxw=/fit-in/900x600/filters:no_upscale():strip_icc()/pic3880340.jpg",
+  },
+];
 
-
-export const products =[
-    {
-        name: "Blood Rage",
-        price: 50.00,
-        image: "https://cf.geekdo-images.com/HkZSJfQnZ3EpS214xtuplg__imagepage/img/nLp0poXg-Y6szkicHe7U2thnwhk=/fit-in/900x600/filters:no_upscale():strip_icc()/pic2439223.jpg"
-    },
-    {
-        name: "Rising Sun",
-        price: 60.00,
-        imaga: "https://cf.geekdo-images.com/iwevA6XmiNLHn1QnGUucqw__imagepage/img/W6vxWVStCBL7qs6yW_G0VbC9Jxw=/fit-in/900x600/filters:no_upscale():strip_icc()/pic3880340.jpg"
-    }
-]
-
-export const items =[
-    {
-        name: "Blood Rage",
-        price: 50.00,
-        image: "https://cf.geekdo-images.com/HkZSJfQnZ3EpS214xtuplg__imagepage/img/nLp0poXg-Y6szkicHe7U2thnwhk=/fit-in/900x600/filters:no_upscale():strip_icc()/pic2439223.jpg"
-    },
-    {
-        name: "Rising Sun",
-        price: 60.00,
-        imaga: "https://cf.geekdo-images.com/iwevA6XmiNLHn1QnGUucqw__imagepage/img/W6vxWVStCBL7qs6yW_G0VbC9Jxw=/fit-in/900x600/filters:no_upscale():strip_icc()/pic3880340.jpg"
-    }
-]
+export const items = [
+  {
+    name: "Blood Rage",
+    price: 50.0,
+    image:
+      "https://cf.geekdo-images.com/HkZSJfQnZ3EpS214xtuplg__imagepage/img/nLp0poXg-Y6szkicHe7U2thnwhk=/fit-in/900x600/filters:no_upscale():strip_icc()/pic2439223.jpg",
+  },
+  {
+    name: "Rising Sun",
+    price: 60.0,
+    image:
+      "https://cf.geekdo-images.com/iwevA6XmiNLHn1QnGUucqw__imagepage/img/W6vxWVStCBL7qs6yW_G0VbC9Jxw=/fit-in/900x600/filters:no_upscale():strip_icc()/pic3880340.jpg",
+  },
+];
